f0d8fa1d7eed2d1b41243c125e9c4b9f0b9694ca
zilya <zilyamuslimova3@gmail.com>
2022-03-29 11:05:27 +0400

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String Hello = "Hello";
+        String World = "world";
+        System.out.println(Hello + " " + World);
+    }
+}
